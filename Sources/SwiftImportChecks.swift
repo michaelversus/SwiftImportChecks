814f2cceb9682aa2e967963899a016106dc4a6c7
Michael <michaliskarag@gmail.com>
2025-05-03 20:07:36 +0300

    Update SwiftImportChecks.swift

--- Sources/SwiftImportChecks.swift
+++ Sources/SwiftImportChecks.swift
@@ -3,23 +3,23 @@ import Foundation
 
 @main
 struct SwiftImportChecks: ParsableCommand {
-    //@Option(name: [.short, .customLong("config")], help: "The path of `.sic.yml`.")
-    var configurationPath: String? = "/Users/m.karagiorgos/iOSNative/.sic.yml"
+    @Option(name: [.short, .customLong("config")], help: "The path of `.sic.yml`.")
+    var configurationPath: String?
 
-    //@Option(name: .shortAndLong, help: "The rootPath of your project. If you omit this, the current directory will be used.")
-    var rootPath: String? = "/Users/m.karagiorgos/iOSNative"
+    @Option(name: .shortAndLong, help: "The rootPath of your project. If you omit this, the current directory will be used.")
+    var rootPath: String?
 
-    // @Option(name: .shortAndLong, help: "The name of your xcodeproj file. For example: MyProject.xcodeproj")
-    var projectFileName: String? //= "Stoiximan.xcodeproj"
+    @Option(name: .shortAndLong, help: "The name of your xcodeproj file. For example: MyProject.xcodeproj")
+    var projectFileName: String?
 
-    // @Option(name: .shortAndLong, help: "The path of your local packages")
-    var spmPackagesPath: String? = "Packages"
+    @Option(name: .shortAndLong, help: "The path of your local packages")
+    var spmPackagesPath: String?
 
-    //@Option(name: .shortAndLong, help: "The name of your target. For example: App")
+    @Option(name: .shortAndLong, help: "The name of your target. For example: App")
     var targetName: String?
 
-    //@Option(name: .shortAndLong, help: "Flag to enable verbose output.")
-    var verbose: Bool = true
+    @Option(name: .shortAndLong, help: "Flag to enable verbose output.")
+    var verbose: Bool = false
 
     func run() throws {
         let compositionRoot = CompositionRoot(
